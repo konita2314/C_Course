aac78e7c126ee94dbf86123ae8209572edd5f3e9
konita2314 <konita2314@163.com>
2025-12-11 14:07:17 +0800

    new courses included

--- py/1stclass.py
+++ py/1stclass.py
@@ -1,5 +1,6 @@
 #靶机后台
 from socket import * #网络相关模块
+import os
 #套接字准备
 main = socket()
 #绑定号码
@@ -11,3 +12,13 @@ main.listen()
 #转接给分套接字
 s , addr = main.accept()
 print(addr)#看一看地址
+
+inp = input("Command?")
+s.send(inp.encode())#发送
+if inp == 1:
+    
+    
+    
+    with open("1.jpg") as file:
+        for data in file:
+            s.send(data)
