c6a9762955e16f629fce74a004f74a8d523c632a
konita2314 <konita2314@163.com>
2025-12-18 14:06:22 +0800

    new course

---
+++ exam4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define N 20
+int main()
+{
+    char text[N] = "characteristic"; // 如果改成输入，可以使用gets(text)。有区别吗？
+    int ch;
+    int i, j;
+    printf("%s\n", text);
+    printf("Delete which chart?\n");
+    ch = getchar();
+    for (i = j = 0; text[i] != '\0'; i++)
+    {
+        if (text[i] != ch)
+        {
+            text[j++] = text[i];
+        }
+    }
+    text[j] = '\0';
+    printf("%s\n", text);
+
+    return 0;
+} 
